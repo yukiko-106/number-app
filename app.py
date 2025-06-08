93f9db9993b3ab96c9c0d40a90f46d5c1e50674a
yukiko-106 <o.yukiko66@gmail.com>
2025-06-09 01:44:36 +0900

    Update app.py

--- app.py
+++ app.py
@@ -159,3 +159,7 @@ def reset():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
+
+import os
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI', 'sqlite:///data.db')
